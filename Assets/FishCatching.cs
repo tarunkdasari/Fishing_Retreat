de505ed8eb10a5ae88d2a87fc11572e6c2afba09
ColinClark383 <116741932+ColinClark383@users.noreply.github.com>
2025-11-16 21:30:30 -0500

    Fish variety added

---
+++ Assets/FishCatching.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishCatching : MonoBehaviour
+{
+    public Transform fishTransform;
+    public GameObject fishPrefab;
+    private GameObject currFish;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Catch()
+    {
+        if (currFish)
+        {
+            return;
+        }
+        currFish = Instantiate(fishPrefab, fishTransform.position, Quaternion.identity);
+        currFish.transform.SetParent(fishTransform);
+        MeshRenderer mr = currFish.GetComponentInChildren<MeshRenderer>();
+        // Get a copy of the material array
+        Material[] mats = mr.materials;
+        mats[2].color = new Color(
+            Random.Range(0.0f, 1.0f),
+            Random.Range(0.0f, 1.0f),
+            Random.Range(0.0f, 1.0f)
+        );
+        mats[3].color = new Color(
+            Random.Range(0.0f, 1.0f),
+            Random.Range(0.0f, 1.0f),
+            Random.Range(0.0f, 1.0f)
+        );
+        mr.materials = mats;
+
+        // --- Optional: Randomize Scale ---
+        float scale = Random.Range(0.7f, 1.4f);
+        currFish.transform.localScale = Vector3.one * scale;
+        Debug.Log("Fish hooked!");
+    }
+}
